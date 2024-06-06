5bddc03b9941a0593f8ec8691067c5721b3077e4
ROHIT SIHMAR <rohit.25072018@gmail.com>
2024-06-06 20:51:22 +0530

    flow-build-prod

---
+++ webpack.prod.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"), // to import index.html file inside index.js
+      title: "Flow Builder",
+      filename: "./index.html",
+    }),
+  ],
+});
